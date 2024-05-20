e9eba975cf9a5c8d8d44255841b498ffbdda1bec
Gabriele Amato <amatogabry250@gmail.com>
2024-05-20 12:38:58 +0200

    boidToShape better written

--- graphics.cpp
+++ graphics.cpp
@@ -1,6 +1,5 @@
 #include "graphics.hpp"
 
 sf::Vector2<float> bd::boidToShape(bd::Boid& b) {
-  sf::Vector2<float> v = {b.get_Pos()[0], b.get_Pos()[1]};
-  return v;
+  return {b.get_Pos_X(), b.get_Pos_Y()};
 }
