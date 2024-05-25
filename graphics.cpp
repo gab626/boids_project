818a57c64e7abe31286272fe56ec3c017f350555
Gabriele Amato <amatogabry250@gmail.com>
2024-05-25 18:24:29 +0200

    Derived BoidShape from ConvexShape

--- graphics.cpp
+++ graphics.cpp
@@ -1,7 +1,38 @@
 #include "graphics.hpp"
 
 using bd::Boid;
+using bd::BoidShape;
 
-sf::Vector2<float> bd::boidToShape(Boid const& b) {
+BoidShape::BoidShape() {
+  this->setPointCount(4);
+  this->setPoint(0, sf::Vector2f(0.f, 5.f));
+  this->setPoint(1, sf::Vector2f(2.f, 0.f));
+  this->setPoint(2, sf::Vector2f(4.f, 5.f));
+  this->setPoint(3, sf::Vector2f(2.f, 4.f));
+  this->setFillColor(sf::Color::Green);
+  this->setOrigin({2.f, 3.f});
+  this->setScale(2.5f, 2.5f);
+}
+
+BoidShape::~BoidShape() {}
+
+std::size_t BoidShape::getPointCount() const { return 4; }
+
+sf::Vector2f BoidShape::getPoint(std::size_t index) const {
+  switch (index) {
+    case 0:
+      return sf::Vector2f(0.f, 5.f);
+    case 1:
+      return sf::Vector2f(2.f, 0.f);
+    case 2:
+      return sf::Vector2f(4.f, 5.f);
+    case 3:
+      return sf::Vector2f(2.f, 4.f);
+    default:
+      return sf::Vector2f(0.f, 0.f);
+  }
+}
+
+sf::Vector2f bd::boidToShape(Boid const& b) {
   return {b.getPositionX(), b.getPositionY()};
 }
--- graphics.hpp
+++ graphics.hpp
@@ -6,9 +6,17 @@
 #include "boid.hpp"
 
 namespace bd {
+// rivedere in dettaglio tutto di questa classe per sicurezza
+class BoidShape : public sf::ConvexShape {
+ public:
+  BoidShape();
+  ~BoidShape();
+  virtual std::size_t getPointCount() const;
+  virtual sf::Vector2f getPoint(std::size_t index) const;
+};
 
-sf::Vector2<float> boidToShape(Boid const&);
+sf::Vector2f boidToShape(Boid const&);
 
-}
+}  // namespace bd
 
 #endif
